5b2f935f6394c5afc814b24ccf770e5dd00a6b57
Bikcode <bikcodeh@gmail.com>
2023-11-29 06:56:34 -0500

    create navbar component

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,12 @@
 import { Box } from "@mui/material";
+import { NavBar } from "../components/NavBar";
+
+const drawerWidth = 240
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ displaxy: "flex" }}>
-      {/* Navbar */}
+      <NavBar  drawerWidth={drawerWidth}/>
 
       {/* Sidebar */}
       <Box component="main" sx={{ flexFlow: 1, p: 3 }}>
