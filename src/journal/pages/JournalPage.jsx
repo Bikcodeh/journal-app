2f64767c60f65e114ec4aa0b7e3137a0b719f767
Bikcode <bikcodeh@gmail.com>
2023-11-29 09:01:25 -0500

    partial creation of note view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView } from "../views/NothingSelectedView";
+import { NoteView } from "../views/NoteView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-       <NothingSelectedView />
+       <NoteView />
     </JournalLayout>
   );
 };
---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,38 @@
+import { SaveOutlined } from "@mui/icons-material";
+import { Grid, Button, Typography, TextField } from "@mui/material";
+
+export const NoteView = () => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ mb: 1 }}
+    >
+        <Grid item>
+            <Typography fontSize={30} fontWeight='light'>28 agust, 2023</Typography>
+        </Grid>
+      <Grid item>
+        <Button color="primary" sx={{ padding: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Save
+        </Button>
+      </Grid>
+      <Grid container>
+        <TextField 
+            label="Title"
+            placeholder="Title"
+            fullWidth
+            sx={{ mb: 2}}
+        />
+        <TextField 
+            placeholder="What happened to you today?"
+            fullWidth
+            multiline
+            rows={5}
+        />
+      </Grid>
+    </Grid>
+  );
+};
