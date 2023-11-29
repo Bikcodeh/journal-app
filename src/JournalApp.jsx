984181cf480a1f4f365832fe9fe21f55ca76aaee
Bikcode <bikcodeh@gmail.com>
2023-11-28 21:41:21 -0500

    create partial routes to handle navigation in app

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,7 @@
+import { AppRouter } from "./router/AppRouter";
+
 export const JournalApp = () => {
   return (
-    <>JournalApp</>
+    <AppRouter />
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles.css";
 
 import { JournalApp } from "./JournalApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <JournalApp />
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
