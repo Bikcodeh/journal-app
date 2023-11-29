010033e6c688bd00a360ec0f4c5260194632d37a
Bikcode <bikcodeh@gmail.com>
2023-11-29 08:41:29 -0500

    create view nothing selected

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
 import { Typography } from "@mui/material";
-import { MailOutline } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-       <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography>
+       <NothingSelectedView />
     </JournalLayout>
   );
 };
