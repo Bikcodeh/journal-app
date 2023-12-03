a964c007950d8e22514574fcf3c4a8406e8e94a2
Bikcode <bikcodeh@gmail.com>
2023-12-03 09:06:24 -0500

    implements create a note with cloud firestore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,25 +1,35 @@
-import { Typography, IconButton } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView } from "../views/NothingSelectedView";
 import { NoteView } from "../views/NoteView";
 import { AddOutlined } from "@mui/icons-material";
+import { startAddNewNote } from "../../store/journal/thunks";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onAddNewNote = () => {
+    dispatch(startAddNewNote());
+  };
   return (
     <JournalLayout>
-       {/* <NoteView /> */}
-       <NothingSelectedView />
-       <IconButton size="large"
-       sx={{
-        color: 'white',
-        backgroundColor: 'error.main',
-        ':hover': { backgroundColor: 'error.main', opacity: 0.8},
-        position: 'fixed',
-        right: 50,
-        bottom: 50
-       }}>
-          <AddOutlined sx={{ fontSize: 30 }} />
-       </IconButton>
+      {/* <NoteView /> */}
+      <NothingSelectedView />
+      <IconButton
+      onClick={onAddNewNote}
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.8 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
