08d4b60e4c56e7876a72966729f2f999767d1bb8
Bikcode <bikcodeh@gmail.com>
2023-12-09 19:46:12 -0500

    tests for startGoogleSignIn success and error cases

--- tests/stores/auth/thunks.test.js
+++ tests/stores/auth/thunks.test.js
@@ -1,5 +1,7 @@
-import { checkingCredentials } from "../../../src/store/auth/authSlice";
-import { checkingAuthentication } from "../../../src/store/auth/thunks";
+import { signInWithGoogle } from "../../../src/firebase/providers";
+import { checkingCredentials, login, logout } from "../../../src/store/auth/authSlice";
+import { checkingAuthentication, startGoogleSignIn } from "../../../src/store/auth/thunks";
+import { demoUser  } from "../../fixtures/authFixtures";
 
 jest.mock('../../../src/firebase/providers');
 
@@ -12,4 +14,24 @@ describe('Tests for AuthThunks', () => {
         await checkingAuthentication('dsda',  'sdsd')(dispatch);
         expect(dispatch).toHaveBeenCalledWith(checkingCredentials({email: 'dsda', password: 'sdsd'}));
     });
+
+    test('startGoogleSignIn should call signInWithGoogle successfully', async () => { 
+        const dataLogin = { ok: true, ...demoUser }
+        await signInWithGoogle.mockResolvedValue(dataLogin);
+
+        await startGoogleSignIn()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(login(dataLogin));
+     });
+
+     test('startGoogleSignIn should call signInWithGoogle error', async () => { 
+        const dataLogin = { ok: false, errorMessage: 'error login' }
+        await signInWithGoogle.mockResolvedValue(dataLogin);
+
+        await startGoogleSignIn()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(logout());
+     });
 });
