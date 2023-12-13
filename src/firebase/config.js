06ba37606194b81ffaab77ae4b789e16e61352d3
Bikcode <bikcodeh@gmail.com>
2023-12-13 08:35:37 -0500

    set environment variables for prod and test scopes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,42 +4,27 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 import { getAnalytics } from "firebase/analytics";
+import { getEnvironments } from "../helpers/getEnvironments";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-/*const firebaseConfig = {
-
-    apiKey: "AIzaSyA-xlzxEO_NiuE36uzy-2NOk-qwBjeX4ew",
-    authDomain: "journal-app-react-91dfa.firebaseapp.com",
-    projectId: "journal-app-react-91dfa",
-    storageBucket: "journal-app-react-91dfa.appspot.com",
-    messagingSenderId: "934376401565",
-    appId: "1:934376401565:web:a070743729834b4b4b0274",
-    measurementId: "G-V0SV4PJP91"
-};*/
-
+const {
+    VITE_APIKEY,
+    VITE_AUTHDOMAIN,
+    VITE_PROJECTID,
+    VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID,
+    VITE_APPID,
+    VITE_MEASUREMENTID } = getEnvironments();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_vfsG8zOdrgBprpTT3LyvHHqRPnSdrP4",
-    authDomain: "react-testing-83544.firebaseapp.com",
-    projectId: "react-testing-83544",
-    storageBucket: "react-testing-83544.appspot.com",
-    messagingSenderId: "8849441372",
-    appId: "1:8849441372:web:f0f9a5a3395263cb1edc99",
-    measurementId: "G-HNGVLZ6FW2"
+    apiKey: VITE_APIKEY,
+    authDomain: VITE_AUTHDOMAIN,
+    projectId: VITE_PROJECTID,
+    storageBucket: VITE_STORAGEBUCKET,
+    messagingSenderId: VITE_MESSAGINGSENDERID,
+    appId: VITE_APPID,
+    measurementId: VITE_MEASUREMENTID 
 };
-
-
-
-// Initialize Firebase
 
 export const FirebaseApp = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth(FirebaseApp);
